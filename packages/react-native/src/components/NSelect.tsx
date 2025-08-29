8f6c1af382d1cfa474500a0e81d0dd7fae784a89
ursnj <niranjan.devasani@gmail.com>
2025-08-29 14:21:19 +0530

    Nayan UI

--- packages/react-native/src/components/NSelect.tsx
+++ packages/react-native/src/components/NSelect.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { ScrollView, View } from 'react-native';
+import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { NText } from '@/components/NText';
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from '@/components/ui/select';
@@ -56,7 +56,7 @@ export const NSelect = React.memo<NSelectProps>(
             <SelectValue className="text-text text-sm native:text-lg" placeholder={placeholder} />
           </SelectTrigger>
           <SelectContent insets={contentInsets} className={cn('w-full bg-card border-border shadow', inputClassName)}>
-            <ScrollView style={{ maxHeight: 250 }} showsVerticalScrollIndicator={true} nestedScrollEnabled={true}>
+            <View className="max-h-[250px] overflow-scroll">
               <SelectGroup>
                 {selectLabel && <SelectLabel className="text-text">{selectLabel}</SelectLabel>}
                 {items.map(item => (
@@ -65,7 +65,7 @@ export const NSelect = React.memo<NSelectProps>(
                   </SelectItem>
                 ))}
               </SelectGroup>
-            </ScrollView>
+            </View>
           </SelectContent>
         </Select>
       </View>
