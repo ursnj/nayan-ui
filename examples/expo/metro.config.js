6c3f540c11c8727c3c8bb12658f71da79e29ccde
ursnj <niranjan.devasani@gmail.com>
2026-03-24 03:50:57 +0530

    Migrate to Hero UI

--- examples/expo/metro.config.js
+++ examples/expo/metro.config.js
@@ -1,6 +1,16 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
-const { withNativeWind } = require('nativewind/metro');
+const { withUniwindConfig } = require('uniwind/metro');
 
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
 
-module.exports = withNativeWind(config, { input: './global.css' });
+const config = getDefaultConfig(projectRoot);
+
+// Watch the monorepo packages folder
+config.watchFolders = [monorepoRoot];
+
+// Resolve node_modules from both the project and monorepo root
+config.resolver.nodeModulesPaths = [path.resolve(projectRoot, 'node_modules'), path.resolve(monorepoRoot, 'node_modules')];
+
+module.exports = withUniwindConfig(config, { cssEntryFile: './global.css' });
--- examples/expo/src/app/_layout.tsx
+++ examples/expo/src/app/_layout.tsx
@@ -2,13 +2,13 @@ import 'react-native-reanimated';
 import { NTheme, THEMES, useNTheme } from '@nayan-ui/react-native';
 import { Stack } from 'expo-router';
 import '../../global.css';
-import { components, themeColors } from '../constants';
+import { components } from '../constants';
 
 const RootLayout = () => {
   const { theme } = useNTheme();
 
   return (
-    <NTheme theme={theme || THEMES.light} themeColors={themeColors}>
+    <NTheme theme={theme || THEMES.light}>
       <Stack>
         <Stack.Screen name="index" options={{ title: 'React Native Nayan' }} />
         {components.map((component: any) => (
