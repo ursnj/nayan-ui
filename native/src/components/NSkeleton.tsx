fd1261b66bf218943890c40716f4f40addca82a3
ursnj <niranjan.devasani@gmail.com>
2026-05-04 10:03:45 +0530

    Use Hero UI

--- native/src/components/NSkeleton.tsx
+++ native/src/components/NSkeleton.tsx
@@ -5,7 +5,7 @@ import { cn } from '../helpers/utils';
 export interface NSkeletonProps extends SkeletonProps {}
 
 export const NSkeleton = React.memo<NSkeletonProps>(({ className, ...props }) => {
-  return <Skeleton className={cn('bg-surface', className)} {...props} />;
+  return <Skeleton className={cn(className)} {...props} />;
 });
 
 NSkeleton.displayName = 'NSkeleton';
