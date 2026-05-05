f38cd15d61fea5c2f61a085e9fc08625ca401001
ursnj <niranjan.devasani@gmail.com>
2026-05-06 02:37:42 +0530

    Use Hero UI

--- packages/native/example/metro.config.js
+++ packages/native/example/metro.config.js
@@ -4,6 +4,7 @@ const { withMetroConfig } = require('react-native-monorepo-config');
 const { withUniwindConfig } = require('uniwind/metro');
 
 const root = path.resolve(__dirname, '..');
+const monorepoRoot = path.resolve(__dirname, '../../..');
 
 /**
  * Metro configuration
@@ -15,6 +16,17 @@ const config = withMetroConfig(getDefaultConfig(__dirname), {
   root,
   dirname: __dirname
 });
+
+// Add monorepo root node_modules for hoisted dependency resolution
+config.watchFolders = [
+  ...(config.watchFolders || []),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
+config.resolver.nodeModulesPaths = [
+  path.resolve(__dirname, 'node_modules'),
+  path.resolve(root, 'node_modules'),
+  path.resolve(monorepoRoot, 'node_modules'),
+];
 
 module.exports = withUniwindConfig(config, {
   cssEntryFile: './src/global.css',
