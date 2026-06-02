c68c536d55b408db0aa1fc530a35cdf1c934f7c1
ursnj <niranjan.devasani@gmail.com>
2026-06-03 01:49:02 +0530

    Fix examples

--- examples/nextjs/src/app/layout.tsx
+++ examples/nextjs/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from 'next';
-import ThemeWrapper from './ThemeWrapper';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -14,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
-        <ThemeWrapper>{children}</ThemeWrapper>
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
